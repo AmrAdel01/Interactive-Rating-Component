c2eeb000897a96bc0276566104718484732da416
AmrAdel <86123092+AmrAdel01@users.noreply.github.com>
2022-10-10 16:19:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 "use strict";
 
+
+<!--List Item-->
 const btns = document.querySelectorAll(".rating_btn");
 const submit = document.querySelector(".sub_btn");
 const removeCardRating = document.querySelector(".card-rating");
 const showModel = document.getElementById("model");
+<!--Rates-->
 const selectedRating = document.querySelector(".selected-rating");
+
+let RateValue =0;
+
+btns.forEach( (btn) => btn.addEventListener('click', getRate));
+
+function getRate (e) {
+    RateValue = e.currentTarget.textContent;
+}
+    
+
+
 
 const removeCard = function () {
   removeCardRating.classList.add("hidden");
@@ -12,6 +26,7 @@ const removeCard = function () {
 submit.addEventListener("click", function () {
   removeCard();
   showFeedBack();
+  selectedRating.textContent=`${RateValue}`;
 });
 function showFeedBack() {
   showModel.removeAttribute("hidden");
